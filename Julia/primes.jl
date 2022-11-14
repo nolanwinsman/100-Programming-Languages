81a910075ab91beb19477fa8661d6b29aae219da
Nolan <68390327+nolanwinsman@users.noreply.github.com>
2022-11-14 10:43:23 -0700

    MultiProcessing started. Needs to work

--- Julia/primes.jl
+++ Julia/primes.jl
@@ -1,4 +1,6 @@
 using Printf
+using Distributed
+
 
 # calculates if n is a prime number
 function prime(n)
@@ -9,20 +11,24 @@ function prime(n)
 		# n is divisible by an integer other than 1 and itself
 		if n % i == 0
 			@printf("%d is prime, divisible by %d\n", n, i)
-			return true
+			
 		end
 	end
 	@printf("%d is not prime\n", n)
 	return false
 end
 
+function primes_to_n(n)
+	@distributed for i in 1:n
+		prime(i)
+	end
+end
+
 # main function to calculate if numbers 1 to 1000000 are prime
 function main()
 	# used to determine how long it takes to calculate the prime numbers
 	@time begin
-		for i in 1:1000000
-			prime(i)
-		end
+		primes_to_n(100_000)
 	end
 end
 
