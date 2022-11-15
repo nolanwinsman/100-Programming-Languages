7c021356e743a47f1a8f18cffd3b678bf89b48b1
Nolan <68390327+nolanwinsman@users.noreply.github.com>
2022-11-14 17:58:40 -0700

    Timing added to programs. Multiprocessing still needs to be fixed

--- Julia/primes.jl
+++ Julia/primes.jl
@@ -1,6 +1,6 @@
 using Printf
 using Distributed
-
+using BenchmarkTools
 
 # calculates if n is a prime number
 function prime(n)
@@ -10,15 +10,27 @@ function prime(n)
 		end
 		# n is divisible by an integer other than 1 and itself
 		if n % i == 0
-			@printf("%d is prime, divisible by %d\n", n, i)
-			
+			#@printf("%d is prime, divisible by %d\n", n, i)
+			return true
 		end
 	end
-	@printf("%d is not prime\n", n)
+	#@printf("%d is not prime\n", n)
 	return false
 end
 
-function primes_to_n(n)
+function normal_calculating_primes(n)
+		for i in 1:n
+			prime(i)
+		end
+end
+
+function multi_threading_primes(n)
+		Threads.@threads for i in 1:n
+			prime(i)
+		end
+end
+
+function multi_processing_primes(n)
 	@distributed for i in 1:n
 		prime(i)
 	end
@@ -26,10 +38,13 @@ end
 
 # main function to calculate if numbers 1 to 1000000 are prime
 function main()
-	# used to determine how long it takes to calculate the prime numbers
-	@time begin
-		primes_to_n(100_000)
-	end
+	n = 10_000
+	println("Normal")
+	@btime normal_calculating_primes($n)
+	println("Multithreading")
+	@btime multi_threading_primes($n)
+	println("Multiprocessing")
+	# @btime multi_processing_primes($n)
 end
 
 main() # calls the main function
